39d17e4e042d7cc0109500b610f4febd5a968438
Sevenquarters <yangpeiqi2003@njust.edu.cn>
2025-10-16 11:08:04 +0800

    jest测试包推送

---
+++ timesTwo.js
@@ -0,0 +1,9 @@
+// Task 1: Code the timesTwo function declaration
+// WRITE YOUR CODE HERE - Create a function named timesTwo that takes a number and returns the value multiplied by 2
+
+// Task 2: Export the timesTwo function as a module
+// WRITE YOUR CODE HERE - Use module.exports to export the timesTwo function
+function timesTwo(num) {
+    return num*2
+}
+module.exports = timesTwo;
